d31a62ac29b623c4b41cfa097ea4e65248086d39
antonym0us <hak1@williams.edu>
2017-05-01 14:30:04 -0400

    Update Teller.java

--- Teller.java
+++ Teller.java
@@ -6,11 +6,11 @@ public class Teller {
     protected PriorityQueue<Customer> customers;
     protected Customer currentClient;
     protected int time;
-    
+
     /** Constructor */
     public Teller (PriorityQueue<Customer> customers) {
-        this.customers = customers;
-        isIdle = true;
+	this.customers = customers;
+	isIdle = true;
     }
 
     /** */
@@ -20,20 +20,31 @@ public class Teller {
 	if (isIdle && !customers.isEmpty() && time >= customers.getFirst().getEventTime()) {
 	    currentClient = customers.remove();
 	}
-	
+
 	if (currentClient != null) {
 	    // Service the client for a single timestep
 	    currentClient.service();
+
 	    
+
 	    // Is the client satisfied?
-	    isIdle = (currentClient.getServiceTime() <= 0);
+	    if (currentClient.getServiceTime() <= 0) {
+		isIdle = true;
+		currentClient = null;
+	    } else {
+		isIdle = false;
+	    }
 	}
+    }
+
+    public PriorityQueue<Customer> getCustomers(){
+	return customers;
     }
     
     public boolean isAvailable() {
-        return isIdle;
+	return isIdle;
     }
-
+    
     public void updateTime(int time) {
 	this.time = time;
     }
@@ -43,11 +54,10 @@ public class Teller {
 	if (currentClient == null) {
 	    result = "Serving no customers";
 	} else {
-	    result = "isIdle: " + isIdle + "\n" +
-		"Customer arrived at " + currentClient.getEventTime() + ", serviceTime is " + currentClient.getServiceTime();
+	        result = "isIdle: " + isIdle + "\n" +
+		    "Customer arrived at " + currentClient.getEventTime() + ", serviceTime is " + currentClient.getServiceTime();
 	}
 	return result;
     }
-    
-}
 
+}
