ecf465cd47959cae3b8d04ab7cabf529f62bb5c9
antonym0us <hak1@williams.edu>
2017-05-01 11:06:30 -0400

    Update Teller.java

--- Teller.java
+++ Teller.java
@@ -3,20 +3,30 @@ import structure5.*;
 public class Teller {
 
     protected boolean isIdle;
-    protected PriorityQueue<Customer> customers;
+    protected Queue<Customer> customers;
     protected Customer currentClient;
     
-    public Teller (PriorityQueue<Customer> customers) {
-        this.customers = customers;
-        isIdle = true;
+    public Teller (Queue<Customer> customers) {
+	this.customers = customers;
+	isIdle = true;
     }
-
-    public void serviceCustomer(Customer nextClient) {
-        if (currentClient == null) {currentClient = nextClient;}
-        nextClient.service();
-    }
-
+    
+    public void serviceCustomer(){
+	if (!customers.isEmpty()) {
+	    currentClient = customers.getFirst();
+	    if (currentClient.getServiceTime() == 1) {
+	    	currentClient.service();
+	    	customers.remove();
+	    	isIdle = true;
+	    } else {
+	    	currentClient.service();
+	    	isIdle = false;
+	    }
+    	}   
+    }    
+    
+    
     public boolean isAvailable() {
-        return isIdle;
+    	return isIdle;
     }
 }
